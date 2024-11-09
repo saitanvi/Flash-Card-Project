0d427e98e83c5eca696fe2dc4cd9199451e4486e
Tanvi Sonti <cb.en.u4cys21072@cb.students.amrita.edu>
2024-11-09 10:47:02 +0530

    second commit

--- app/src/main/java/com/example/flashcardapplication/FlashScreenActivity.java
+++ app/src/main/java/com/example/flashcardapplication/FlashScreenActivity.java
@@ -1,14 +1,12 @@
 package com.example.flashcardapplication;
 
-
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FlashScreenActivity extends AppCompatActivity {
-    @Override
+    @Overridea
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flash_screen);
